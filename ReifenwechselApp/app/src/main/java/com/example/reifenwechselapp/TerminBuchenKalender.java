f916797dc1e81cf032fb36ab285bc30c47e594e8
kaani65 <kaan65md@hotmail.de>
2021-01-05 15:02:04 +0100

    Add files via upload

---
+++ ReifenwechselApp/app/src/main/java/com/example/reifenwechselapp/TerminBuchenKalender.java
@@ -0,0 +1,14 @@
+package com.example.reifenwechselapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TerminBuchenKalender extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_termin_buchen_kalender);
+    }
+}
